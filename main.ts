03f6b2ff2d1087f7d88c6e580294205340c8680f
default <ketia.ineza@stu.ocsb.ca>
2025-11-30 00:50:19 +0000

    Purchasing Items

--- main.ts
+++ main.ts
@@ -4,9 +4,9 @@ const NUT_COST = 5;
 const WASHER_COST = 3;
 
 // User inputs
-let bolts = Number(prompt("How many bolts would you like to purchase?"));
-let nuts = Number(prompt("How many nuts would you like to purchase?"));
-let washers = Number(prompt("How many washers would you like to purchase?"));
+const bolts = Number(prompt("How many bolts would you like to purchase?"));
+const nuts = Number(prompt("How many nuts would you like to purchase?"));
+const washers = Number(prompt("How many washers would you like to purchase?"));
 
 // Display quantities
 console.log("Number of bolts:               " + bolts);
@@ -14,11 +14,22 @@ console.log("Number of nuts:                " + nuts);
 console.log("Number of washers:         " + washers);
 
 // Order checking
+let orderOk = true;
+
 if (nuts < bolts) {
   console.log("Check the Order - not enough nuts for the bolts you purchased.");
+  orderOk = false;
 }
 
-// Total cost calculation
-// Use teacher’s logic: add 50 cents for nuts instead of 25
-let totalCost = (bolts * BOLT_COST) + (bolts * NUT_COST) + (washers * WASHER_COST);
+if (washers < bolts) {
+  console.log("Check the Order - not enough washers for the bolts you purchased.");
+  orderOk = false;
+}
+
+if (orderOk) {
+  console.log("Order is OK.");
+}
+
+// Total cost
+const totalCost = (bolts * BOLT_COST) + (bolts * NUT_COST) + (washers * WASHER_COST);
 console.log("Your total cost of the order is 175 cents.");
